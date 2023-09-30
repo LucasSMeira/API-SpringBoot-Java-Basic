864faf7ae8c64ac4d84de7d9f289c95c55bdda3e
Lucas Silva Meira <lucasmeiraprofessional@gmail.com>
2023-09-30 07:02:53 -0300

    Final - test

---
+++ api/src/test/resources/data.sql
@@ -0,0 +1,11 @@
+-- Inserindo dados na tabela pessoa
+-- Inserindo dados na tabela pessoa
+INSERT INTO PESSOA (NOME, DATANASCIMENTO) VALUES
+('João Silva', '1990-01-01'),
+('Maria Oliveira', '1992-02-02');
+
+
+-- Inserindo dados na tabela endereco
+INSERT INTO testedb.ENDERECO (id, logradouro, cep, numero, cidade) VALUES
+(1, 'Rua Exemplo 1', '12345-678', '100', 'Cidade Exemplo'),
+(2, 'Rua Exemplo 2', '23456-789', '200', 'Cidade Exemplo');
