bf2e97b257678e308e3b61141c8d9033207b38a0
Lucas Silva Meira <lucasmeiraprofessional@gmail.com>
2023-09-30 01:50:13 -0300

    inserts no banco de dados

---
+++ api/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- Inserindo dados na tabela pessoa
+INSERT INTO pessoa (id, nome, data_nascimento) VALUES
+(1, 'João Silva', '1990-01-01'),
+(2, 'Maria Oliveira', '1992-02-02');
+
+-- Inserindo dados na tabela endereco
+INSERT INTO endereco (id, logradouro, cep, numero, cidade) VALUES
+(1, 'Rua Exemplo 1', '12345-678', '100', 'Cidade Exemplo'),
+(2, 'Rua Exemplo 2', '23456-789', '200', 'Cidade Exemplo');
